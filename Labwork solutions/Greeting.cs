553f339c4acebf0cc4119b2685f3c808c69fc2b8
Kewang <kewang.h.limbu@gmail.com>
2022-05-05 09:56:23 +0545

    LabWork soln added

---
+++ Labwork solutions/Greeting.cs
@@ -0,0 +1,21 @@
+//1. Write a C# program to check if user says "Hi" or "Hello"?
+
+   public class check
+{
+/*
+public  static void greeting()
+you can't access static members with instance syntax.
+
+*/
+
+public  void greeting()
+{
+           
+         Console.WriteLine("Enter 'Hi' or 'Hello': ");
+         string user_input = Console.ReadLine();
+         var result= (user_input=="Hi" || user_input=="Hello") ? $"\nUser Greet saying '{user_input}'" : "\nPlease  entered again!";
+         Console.WriteLine(result);
+         
+       }
+   } 
+
---
+++ Labwork solutions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// using  Greet;
+
+namespace LabAssignment
+{
+
+    class program
+   {
+         public static void Main(string[] args)
+       {   
+        // 1.To check the if user says "Hi" or "Hello"
+        // check c = new check();
+        // c.greeting();
+       
+
+        // 2.To check user entered vowel or not.
+        // alphabet_check a = new alphabet_check();
+        // a.check_vowel();
+       
+        // 3.To check even or odd number
+        // check_num n = new check_num();
+        // n.check_EvenNum();
+
+
+        }
+    }
+}
