d05bbdb9e7cabc8a2a9ee46faef7632914683a78
Kewang <kewang.h.limbu@gmail.com>
2022-05-05 13:06:21 +0545

    Question 5 added

--- Labwork solutions/Program.cs
+++ Labwork solutions/Program.cs
@@ -22,13 +22,16 @@ namespace LabAssignment
         // 3.To check even or odd number
         // check_num n = new check_num();
         // n.check_EvenNum();
-        
+
 
         // 4.To find largest number
         // LargestNum lnum = new LargestNum();
         // lnum.checkLargest();
 
-
+        // 5.To calculate sum of square of n num
+        // SumoFSquare obj =new SumoFSquare();
+        // int n  =Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine(obj.sum_result(n));
 
         }
     }
