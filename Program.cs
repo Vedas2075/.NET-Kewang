b59cf7ea88a34b567caa07f913068f198caeca32
Kewang <kewang.h.limbu@gmail.com>
2022-05-07 14:37:45 +0545

    Question 7 soln added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-// using  Greet;
-// using vowel;
-// using number;
 
-namespace A
+// using  Greet;
+
+namespace LabAssignment
 {
 
     class program
@@ -11,12 +10,44 @@ namespace A
          public static void Main(string[] args)
        {   
         // 1.To check the if user says "Hi" or "Hello"
-        check c = new check();
-        c.greeting();
+        // check c = new check();
+        // c.greeting();
        
 
+        // 2.To check user entered vowel or not.
+        // alphabet_check a = new alphabet_check();
+        // a.check_vowel();
+
        
-       
+        // 3.To check even or odd number
+        // check_num n = new check_num();
+        // n.check_EvenNum();
+
+
+        // 4.To find largest number
+        // LargestNum lnum = new LargestNum();
+        // lnum.checkLargest();
+
+        // 5.To calculate sum of square of n num
+        // SumoFSquare obj =new SumoFSquare();
+        // int n  =Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine(obj.sum_result(n));
+
+
+        // 6.To calculate avg of 3 decimal arguments
+        // AverageNum avg = new AverageNum();
+        // avg.showresult();
+
+
+        // 7.To calculate avg of n arguments
+        AverageNum_nArgs avg = new AverageNum_nArgs();
+        avg.showresult();
+
+
+
+
+
+
         }
     }
 }
